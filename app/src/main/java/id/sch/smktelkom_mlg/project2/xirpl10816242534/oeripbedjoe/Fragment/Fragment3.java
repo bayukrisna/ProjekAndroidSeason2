b8c89d0f63e986c081cb3429597ca3c52eb5fb05
Bayu Krisna <bayu_krisna_24rpl@student.smktelkom-mlg.sch.id>
2017-03-20 12:45:42 +0700

    Layout Fragment 3

--- app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl10816242534/oeripbedjoe/Fragment/Fragment3.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl10816242534/oeripbedjoe/Fragment/Fragment3.java
@@ -1,16 +1,16 @@
 package id.sch.smktelkom_mlg.project2.xirpl10816242534.oeripbedjoe.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
 
+import id.sch.smktelkom_mlg.project2.xirpl10816242534.oeripbedjoe.MainActivity;
 import id.sch.smktelkom_mlg.project2.xirpl10816242534.oeripbedjoe.R;
 import id.sch.smktelkom_mlg.project2.xirpl10816242534.oeripbedjoe.Recycler.AdapterFragment3;
 import id.sch.smktelkom_mlg.project2.xirpl10816242534.oeripbedjoe.Recycler.Data;
@@ -21,14 +21,20 @@ import id.sch.smktelkom_mlg.project2.xirpl10816242534.oeripbedjoe.Recycler.Data;
 
 public class Fragment3 extends Fragment {
 
+    public static final String ANGT = "angt";
     AdapterFragment3 mIDataAdapterFrag3;
 
     //Menjadikan fragment_3 layout yang dimiliki oleh Fragment3.java
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_3, null);
-        return v;
+        View rootView = inflater.inflate(R.layout.fragment_3, container, false);
+        return rootView;
+    }
+
+    public void goToAttract(View v) {
+        Intent intent = new Intent(getActivity().getApplication(), MainActivity.class);
+        startActivity(intent);
     }
 
     //Membuat RecyclerView
@@ -36,12 +42,19 @@ public class Fragment3 extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+        getView().findViewById(R.id.head).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hasilIntent(1);
+            }
+        });
+    }
 
-        RecyclerView rvChord = (RecyclerView) getView().findViewById(R.id.mRecyclerFrag3);
-        rvChord.setLayoutManager(new GridLayoutManager(this.getActivity(), 2));
-        mIDataAdapterFrag3 = new AdapterFragment3(this.getActivity(), getFrag3List());
-        rvChord.setAdapter(mIDataAdapterFrag3);
+    private void hasilIntent(int i) {
+        Intent intent = new Intent(getActivity().getApplication(), Fragment4.class);
+        intent.putExtra(ANGT, i);
 
+        startActivity(intent);
     }
 
 
