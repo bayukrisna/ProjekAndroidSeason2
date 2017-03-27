183d9f19f5bac9f2972bf649e65ab1e1b59119c9
Ladinar Nanda Aprilia <ladinarnanda@gmail.com>
2017-03-27 22:46:23 +0700

    Themes

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl10816242534/oeripbedjoe/Databasehelper.java
@@ -0,0 +1,8 @@
+package id.sch.smktelkom_mlg.project2.xirpl10816242534.oeripbedjoe;
+
+/**
+ * Created by Smktelkom on 3/22/2017.
+ */
+
+public class Databasehelper {
+}
--- app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl10816242534/oeripbedjoe/ViewActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl10816242534/oeripbedjoe/ViewActivity.java
@@ -1,8 +1,6 @@
 package id.sch.smktelkom_mlg.project2.xirpl10816242534.oeripbedjoe;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -55,14 +53,6 @@ public class ViewActivity extends AppCompatActivity {
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         int id = getIntent().getIntExtra(MainActivity.ID, 0);
 
